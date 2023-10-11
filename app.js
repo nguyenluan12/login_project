cda20244f6dafbab502a238f393a507d08072e87
luan <huuluan0511@gmail.com>
2023-10-11 23:47:46 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// showpassword
 let showPassword = document.getElementById('showPassword')
 let inputPassword = document.getElementById('password')
 showPassword.onclick = function(){
@@ -8,3 +9,19 @@ showPassword.onclick = function(){
     showPassword.classList.remove('show');
   }
 }
+//error email massage
+ let emailInput = document.getElementById('username')
+ let emailError = document.getElementById('email-error')
+ emailInput.addEventListener('blur',()=>{
+  const email = emailInput.value;
+  console.log(email);
+  const emailParttern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if(!emailParttern.test(email)){
+    emailInput.classList.add('error');
+    emailError.textContent= 'Nhập lại email';
+
+  }else{
+    emailInput.classList.remove('error');
+    emailError.textContent='';
+  }
+ });
